3619d0d57e7d392d927a42038a1e40a4f1c76999
Areo Benjamen <areotimileyin1@gmail.com>
2023-07-25 20:18:47 +0100

    (feat): AgileBoardv1 Firebase --init

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
 import ThemeProvider from "./context/ThemeContext.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { initializeApp } from "firebase/app";
+import { FirebaseApp, initializeApp } from "firebase/app";
 import firebaseConfig from "./config/firebase.ts";
 import { getAnalytics } from "firebase/analytics";
-import "firebase/auth";
-import "firebase/firestore";
+import 'firebase/auth'
+import 'firebase/firestore'
 
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App.tsx";
 import "./styles/main.css";
 
-const app = initializeApp(firebaseConfig);
-getAnalytics(app);
+
+const app:FirebaseApp = initializeApp(firebaseConfig)
+getAnalytics(app)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
